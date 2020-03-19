52334d17be36d498bb2500f015921c4b72f84ca6
Philip Johansson <pjohansson01@yahoo.com>
2020-03-19 09:02:59 +0000

    skapade get route

--- index.js
+++ index.js
@@ -4,6 +4,10 @@ const app = express();
 
 app.use(express.static(__dirname + "/public"))
 
+app.get('/', (req, res) => {
+  res.send('hello wrold');
+})
+
 app.listen('3000', () => {
   console.log('server up and running on port: 3000');
 })
